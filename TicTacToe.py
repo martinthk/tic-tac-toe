828e3c0bcd00c2443158c269a429fc355fd4c6e4
Martin T <73326875+martinthk@users.noreply.github.com>
2020-12-08 11:31:02 +0800

    Add files via upload

--- TicTacToe.py
+++ TicTacToe.py
@@ -9,11 +9,11 @@ TODO:
  - Select random function
  - The main function
 """
-
+# Human user will always be the 1st one to start and using 'x'
 board = [' ' for x in range(10)]    # The blank board
 
 
-# * Printing the board
+# * Printing the board * #
 def printBoard(board):
     print(" " + board[1] + "| " + board[2] + "| " + board[3])
     print("---------")
@@ -22,14 +22,14 @@ def printBoard(board):
     print(" " + board[7] + "| " + board[8] + "| " + board[9])
 
 
-# * Check if board is full
-   """
-   Explain:
-   Check if the board is full to end the game
-   If the board is full then its a TIE. 
-   The board can lack to be full with no winner declared and hence a move can be made. 
-   This function will be called every time before a move is made.
-   """
+# * Check if board is full * #
+"""
+Explain:
+Check if the board is full to end the game
+If the board is full then its a TIE. 
+The board can lack to be full with no winner declared and hence a move can be made. 
+This function will be called every time before a move is made.
+"""
 def isBoardFull(board):
     if board.count(" ") > 1:
         return False
@@ -37,22 +37,31 @@ def isBoardFull(board):
         return True
 
 
-# * Check if board is full
-   """
-   Explain:
-   If the board isn’t full, we then check if there is a winner.
-   We declare a winner if 3 “x’s” or 3 “o’s” occur in 1 ROW or in COLUMN or DIAGONALLY.
-   """
+# * Check if board is full * #
+"""
+Explain:
+If the board isn’t full, we then check if there is a winner.
+We declare a winner if 3 “x’s” or 3 “o’s” occur in 1 ROW or in COLUMN or DIAGONALLY.
+"""
 def isWinner(bo, le):
     # "bo" stands for the board.
     # "le" stands for the letter or symbol we're checking.
-    return (bo[7] == le and bo[8] == le and bo[9] == le) or 
+    return ((bo[7] == le and bo[8] == le and bo[9] == le) or 
            (bo[4] == le and bo[5] == le and bo[6] == le) or 
            (bo[1] == le and bo[2] == le and bo[3] == le) or 
            (bo[1] == le and bo[4] == le and bo[7] == le) or  
            (bo[2] == le and bo[5] == le and bo[8] == le) or 
            (bo[3] == le and bo[6] == le and bo[9] == le) or 
            (bo[1] == le and bo[5] == le and bo[9] == le) or  
-           (bo[3] == le and bo[5] == le and bo[7] == le)
+           (bo[3] == le and bo[5] == le and bo[7] == le))
+
+# * Insert letter * #
+""" 
+Explain:
+The user should insert a number position on the board in which 'x' will be planced.
+Number position: 1 - 9 includsive
+"""
+def insertLetter(letter, pos):
+    board[pos] = letter
 
 printBoard(board)
